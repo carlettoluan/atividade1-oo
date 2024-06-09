1bfea6878799350b183b8f5ed83805c7dc01f15d
Luan Carletto <88385522+carlettoluan@users.noreply.github.com>
2024-06-09 13:47:13 -0300

    Update Guitarra.java

--- Guitarra.java
+++ Guitarra.java
@@ -56,13 +56,14 @@ public class Guitarra{
     public void setPreco(double preco) {
         this.preco = preco;
     }
-}
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
     Guitarra minhaGuitarra = new Guitarra("01020304", "fender", "telecaster", "eletrica", "mogno", 1500);
 
-System.out.println(minhaGuitarra.getNumeroSerie);
-System.out.println(minhaGuitarra.getMadeira);
-System.out.println(minhaGuitarra.getPreco);
-System.out.println(minhaGuitarra.getTipo);
+    System.out.println(minhaGuitarra.getNumeroSerie());
+    System.out.println(minhaGuitarra.getMadeira());
+    System.out.println(minhaGuitarra.getPreco());
+    System.out.println(minhaGuitarra.getTipo());
+  }
 }
+
